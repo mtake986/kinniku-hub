23c69538e11921e3a435422d83693add449d72ce
Masahiro Takechi <tak21011@byui.edu>
2022-03-16 18:20:56 -0600

    delete measurement id

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -7,13 +7,12 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCp6krSwy6zODYF_qPWmP_H1vY31PmvUS8",
-  authDomain: "kinnikuhub.firebaseapp.com",
-  projectId: "kinnikuhub",
-  storageBucket: "kinnikuhub.appspot.com",
-  messagingSenderId: "131675559",
-  appId: "1:131675559:web:3ca3bbad263b6be90ff282",
-  measurementId: "G-JRQX0B206R"
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
