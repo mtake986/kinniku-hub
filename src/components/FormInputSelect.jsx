9a482c70161d06df391084302f23a9b28b645205
Masahiro Takechi <tak21011@byui.edu>
2022-03-20 23:03:40 -0600

    create styles for the quiz form

--- src/components/FormInputSelect.jsx
+++ src/components/FormInputSelect.jsx
@@ -4,11 +4,13 @@ const FormInputSelect = props => {
   const { label, onChange, id, ...inputProps } = props;
   return (
     <div className='quizFormInputSelect'>
-      <span>{label}</span>
-      <select name={label} id={label} onChange={onChange}>
+      <label>{label}</label>
+      <select name={label} id={label} onChange={onChange} defaultValue="">
+        <option value="default" disabled>Select a category</option>
         <option value="Workout">Workout</option>
         <option value="Muscle">Muscle</option>
         <option value="Nutrition">Nutrition</option>
+        <option value="Other">Other</option>
       </select>
     </div>
   );
