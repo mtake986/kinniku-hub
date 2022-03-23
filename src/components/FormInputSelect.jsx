464657b14b104a4f56b222d9eb76a3fd38c4eadd
Masahiro Takechi <tak21011@byui.edu>
2022-03-22 23:48:55 -0600

    create a new quiz form with validation with 2 modules (Yup and Formik), I have not done answers field (styling and show errMsg when there is an  empty answer

--- src/components/FormInputSelect.jsx
+++ src/components/FormInputSelect.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
 const FormInputSelect = props => {
-  const { label, onChange, id, ...inputProps } = props;
+  const { label, err, onChange, id, ...inputProps } = props;
   return (
     <div className='quizFormInputSelect'>
       <label>{label}</label>
-      <select name={label} id={label} onChange={onChange}>
-        <option value="">Select a category</option>
+      <select name={label} id={label} onChange={onChange} defaultValue="">
+        <option value="" disabled>Select a category</option>
         <option value="Workout">Workout</option>
         <option value="Muscle">Muscle</option>
         <option value="Nutrition">Nutrition</option>
         <option value="Other">Other</option>
       </select>
+      {/* <span className='quizFormErrMsg'>{err}</span> */}
     </div>
   );
 };
--- src/components/NewQuiz.jsx
+++ src/components/NewQuiz.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import { collection, setDoc, doc, addDoc } from 'firebase/firestore';
-
+import { useNavigate } from 'react-router-dom';
 import FormInputText from './FormInputText';
 import FormInputSelect from './FormInputSelect';
 import db from '../config/firebase';
 
 const NewQuiz = () => {
+  // const navigate = useNavigate();
+  // navigate("/kinniku-quiz/new")
   const [values, setValues] = useState({
     question: '',
     answer1: '',
@@ -14,8 +16,10 @@ const NewQuiz = () => {
     answer4: '',
     correctAnswer: 0,
     createdAt: '',
-    category: ""
+    category: '',
   });
+  const [noValidate, setNoValidate] = useState(true)
+  const [focused, setFocused] = useState(false);
 
   const inputs = [
     {
@@ -24,6 +28,9 @@ const NewQuiz = () => {
       type: 'text',
       placeholder: 'Is protein powder white happy magical snow??',
       label: 'Question',
+      required: true,
+      err: 'Allowed to type only A ~ Z, a ~ z, 0 ~ 9 and 10 to 255 letters',
+      pattern: '^[A-Za-z0-9 ]{10,255}$',
     },
     {
       id: 2,
@@ -31,6 +38,9 @@ const NewQuiz = () => {
       type: 'text',
       placeholder: 'No',
       label: 'Answer 1',
+      required: true,
+      err: 'Allowed to type only A ~ Z, a ~ z, 0 ~ 9 and 10 to 255 letters',
+      pattern: '^[A-Za-z0-9 ]{0,30}$',
     },
     {
       id: 3,
@@ -38,6 +48,9 @@ const NewQuiz = () => {
       type: 'text',
       placeholder: 'Yes',
       label: 'Answer 2',
+      required: true,
+      err: 'Allowed to type only A ~ Z, a ~ z, 0 ~ 9 and 10 to 255 letters',
+      pattern: '^[A-Za-z0-9 ]{0,30}$',
     },
     {
       id: 4,
@@ -45,6 +58,8 @@ const NewQuiz = () => {
       type: 'text',
       placeholder: 'Maybe',
       label: 'Answer 3',
+      err: 'Allowed to type only A ~ Z, a ~ z, 0 ~ 9 and 10 to 255 letters',
+      pattern: '^[A-Za-z0-9 ]{0,30}$',
     },
     {
       id: 5,
@@ -52,29 +67,62 @@ const NewQuiz = () => {
       type: 'text',
       placeholder: 'Tqm',
       label: 'Answer 4',
+      err: 'Allowed to type only A ~ Z, a ~ z, 0 ~ 9 and 10 to 255 letters',
+      pattern: '^[A-Za-z0-9 ]{0,30}$',
     },
     {
       id: 6,
       name: 'correctAnswer',
-      type: 'number',
-      placeholder: 1,
+      type: 'text',
+      placeholder: '',
       label: 'Correct Answer',
+      required: true,
+      err: 'Allowed to type only 1 ~ 4',
+      pattern: '^[1-4]{0,1}$',
     },
     {
       id: 7,
       name: 'category',
+      err: 'Select a category this quiz belongs to.',
       label: 'Category',
     },
   ];
+
+  function SubmitButton() {
+    const category = document.getElementById('Category');
+
+    if (
+      values['question'] &&
+      values['answer1'] &&
+      values['answer2'] &&
+      values['answer3'] &&
+      values['answer4'] &&
+      values['correctAnswer'] &&
+      category.selectedIndex !== 0
+    ) {
+      return <button type='submit'>Submit</button>;
+    } else {
+      return (
+        <button type='submit' disabled>
+          Submit
+        </button>
+      );
+    }
+  }
 
   const onChange = e => {
     setValues({ ...values, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = async e => {
+    setFocused(false);
+    console.log(focused)
     // const quizRef = doc(db, 'quizzes', 'category');
     // await setDoc(quizRef, payload);
     e.preventDefault();
+    console.log('handlesubmit');
+
+    // When all validatations are fine.
     const quizCollectionRef = collection(db, 'quizzes');
     const payload = {
       question: values['question'],
@@ -87,10 +135,11 @@ const NewQuiz = () => {
       correctAnswer: parseInt(values['correctAnswer']),
       likes: 0,
       createdAt: new Date(),
-      category: values['category']
+      category: values['category'],
     };
     await addDoc(quizCollectionRef, payload);
 
+    
     setValues({
       question: '',
       answer1: '',
@@ -99,8 +148,10 @@ const NewQuiz = () => {
       answer4: '',
       correctAnswer: 0,
       createdAt: '',
-      category: "" // how to set it to default value
-    })
+      category: '', // how to set it to default value
+    });
+
+
   };
 
   console.log(values);
@@ -108,15 +159,17 @@ const NewQuiz = () => {
   return (
     <form action='' className='quizForm' onSubmit={handleSubmit}>
       {inputs.map((input, i) => {
-        if (input["type"] === "text" || input["type"] === "number") {
+        if (input['type'] === 'text' || input['type'] === 'number') {
           return (
             <FormInputText
               key={input.id}
               {...input}
               value={values[input.name]}
               onChange={onChange}
+              focused={focused}
+              setFocused={setFocused}
             />
-          )
+          );
         } else {
           return (
             <FormInputSelect
@@ -125,10 +178,10 @@ const NewQuiz = () => {
               value={values[input.name]}
               onChange={onChange}
             />
-          )
+          );
         }
       })}
-      <button>Submit</button>
+      <SubmitButton />
     </form>
   );
 };
