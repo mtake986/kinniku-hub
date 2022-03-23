3c67426d040b9fa620ebc4127ab0af1e7e897579
Masahiro Takechi <tak21011@byui.edu>
2022-03-23 17:05:00 -0600

    prevent duplicated answers and show error message if any duplication.

--- src/components/FormikNewQuiz.jsx
+++ src/components/FormikNewQuiz.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import { Formik, Form, Field, FieldArray } from 'formik';
+import { useState } from 'react';
+import { Formik, Form, Field, FieldArray, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 import { collection, setDoc, doc, addDoc } from 'firebase/firestore';
 import db from '../config/firebase';
+import { ioAddCircleOutline, ioRemoveCircleOutline } from '../icons/icons';
+
+
+Yup.addMethod(Yup.array, 'unique', function(message, mapper = a => a) {
+    return this.test('unique', message, function(list) {
+        return list.length  === new Set(list.map(mapper)).size;
+    });
+});
 
 const quizSchema = Yup.object().shape({
   question: Yup.string()
@@ -11,219 +19,287 @@ const quizSchema = Yup.object().shape({
     .max(250, 'Too Long!')
     .required('Required'),
   answers: Yup.array()
-    .min(2, "grtbg3r4gers")
-    .of(Yup.string().min(0, 'Too Short!').max(50, 'Too Long!').nullable(false))
+    .of(
+      Yup.string()
+        .max(50, 'Too Long!')
+        .required('Required'))
+    .unique('Duplicate answers are not allowed')
     .min(2, `Minimum of 2 answers`),
-  // answer1: Yup.string()
-  //   .min(2, 'Too Short!')
-  //   .max(50, 'Too Long!')
-  //   .required('Required'),
-  // answer2: Yup.string()
-  //   .min(2, 'Too Short!')
-  //   .max(50, 'Too Long!')
-  //   .required('Required'),
-  // answer3: Yup.string()
-  //   .min(2, 'Too Short!')
-  //   .max(50, 'Too Long!')
-  //   .required('Required'),
-  // answer4: Yup.string()
-  //   .min(2, 'Too Short!')
-  //   .max(50, 'Too Long!')
-  //   .required('Required'),
   correctAnswer: Yup.number('only numbers are allowed')
     .min(1, 'type more than 1 please')
     .max(4, 'type less than 5')
     .required('Required'),
-  category: Yup.string().required(),
+  category: Yup.string().required('Required'),
   createdAt: Yup.date(),
   likes: Yup.number(),
 });
 
-export const FormikNewQuiz = () => (
-  <div className='formikNewQuiz'>
-    <Formik
-      initialValues={{
-        question: '',
-        answers: ['', ''],
-        // answer1: '',
-        // answer2: '',
-        // answer3: '',
-        // answer4: '',
-        correctAnswer: '',
-        category: '',
-        createdAt: new Date(),
-        likes: 0,
-      }}
-      validationSchema={quizSchema}
-      onSubmit={async values => {
-        // same shape as initial values
-        console.log(values);
-        const quizCollectionRef = collection(db, 'quizzes');
-        const payload = values;
-        await addDoc(quizCollectionRef, payload);
-      }}
-    >
-      {({ errors, touched, values }) => (
-        <Form>
-          <div style={labelInputContainer}>
-            <label htmlFor='question' style={label}>
-              Question
-            </label>
-            <Field name='question' style={quizFormInputText} />
-            {errors.question && touched.question ? (
-              <div style={quizFormErrMsg}>{errors.question}</div>
-            ) : null}
-          </div>
 
-          <FieldArray
-            name='answers'
-            style={quizFormInputText}
-            render={arrayHelpers => (
-              <div>
-                <label htmlFor='answers1' style={label}>
-                  Answers
-                </label>
-                {values.answers.length <= 3 && (
-                  <button
-                  type='button'
-                  onClick={() => arrayHelpers.push('')}
-                >
-                  {' '}
-                  Add{' '}
-                </button>
-                )}
-                {values.answers && values.answers.length > 0 ? (
-                  values.answers.map((answer, index) => (
-                    <div key={index}>
-                      <Field name={`answers.${index}`} />
-                      <button
-                        type='button'
-                        onClick={() => arrayHelpers.remove(index)}
-                        style={{...values.answers.length <= 2 ? disableToClick : ""}}
-                      >
-                        {' '}
-                        Delete{' '}
-                      </button>
-                    </div>
-                  ))
-                ) : (
-                  <button type='button' onClick={() => arrayHelpers.push('')}>
-                    {' '}
-                    Add an answer
-                  </button>
-                )}
-              </div>
-            )}
-          />
+export const FormikNewQuiz = () => {
+  const [focused, setFocused] = useState("");
 
-          {/* <div style={labelInputContainer}>
-            <label htmlFor="answers1" style={label}>Answers</label>
-            <Field name="answers[0]" style={quizFormInputText} />
-            <Field name="answers[1]" style={quizFormInputText} />
-            <Field name="answers[2]" style={quizFormInputText} />
-            <Field name="answers[3]" style={quizFormInputText} />
-            {errors.answers[0] && touched.answers[0] ? (
-              <div style={quizFormErrMsg}>{errors.answers[0]}</div>
-            ) : null}
-          </div> */}
-
-          {/* <div style={labelInputContainer}>
-            <label htmlFor="answer1" style={label}>Answer 1</label>
-            <Field name="answer1" style={quizFormInputText} />
-            {errors.answer1 && touched.answer1 ? (
-              <div style={quizFormErrMsg}>{errors.answer1}</div>
-            ) : null}
-          </div>
+  return (
+    <div className='formikNewQuiz'>
+      <Formik
+        initialValues={{
+          question: '',
+          answers: ['', ''],
+          correctAnswer: '',
+          category: '',
+          createdAt: new Date(),
+          likes: 0,
+        }}
+        validateOnChange
+        validationSchema={quizSchema}
+        onSubmit={async values => {
+          // same shape as initial values
+          console.log(values);
+          const quizCollectionRef = collection(db, 'quizzes');
+          const payload = values;
           
-          <div style={labelInputContainer}>
-            <label htmlFor="answer2" style={label}>Answer 2</label>
-            <Field name="answer2" style={quizFormInputText} />
-            {errors.answer2 && touched.answer2 ? (
-              <div style={quizFormErrMsg}>{errors.answer2}</div>
-            ) : null}
-          </div>
+          await addDoc(quizCollectionRef, payload);
+        }}
+      >
+        {({ errors, touched, values}) => (
+          <Form>
+            <div style={labelInputContainer}>
+              <label htmlFor='question' style={label}>
+                Question
+              </label>
+              <Field
+                name='question'
+                onFocus={() => {
+                  setFocused("question");
+                }}
+                onBlur={() => {
+                  setFocused("");
+                }}
+                style={focused === "question" ? focusStyle : quizFormInputText }
+              />
+              {errors.question && touched.question ? (
+                <div style={quizFormErrMsg}>{errors.question}</div>
+              ) : null}
+            </div>
 
-          <div style={labelInputContainer}>
-            <label htmlFor="answer3" style={label}>Answer 3</label>
-            <Field name="answer3" style={quizFormInputText} />
-            {errors.answer3 && touched.answer3 ? (
-              <div style={quizFormErrMsg}>{errors.answer3}</div>
-            ) : null}
-          </div>
+            <FieldArray
+              name='answers'
+              style={quizFormInputText}
+              render={arrayHelpers => (
+                <div style={labelInputContainer}>
+                  <label htmlFor='answers1' style={label}>
+                    Answers
+                  </label>
+                  {values.answers.length <= 3 && (
+                    <i
+                      style={moreAnswerIcon}
+                      onClick={() => arrayHelpers.push('')}
+                    >
+                      Add
+                    </i>
+                  )}
+                  {errors.answers && touched.answers && errors.answers === 'Duplicate answers are not allowed' ? (
+                      <div style={quizFormErrMsg}>{errors.answers}</div>
+                    ) : null
+                  }
+                  {values.answers && values.answers.length > 0
+                    ? values.answers.map((answer, index) => (
+                        <div key={index} style={answerContainer}>
+                          <div style={indexAnswerIconContainer}>
+                            <span style={answerIndex}>{index + 1}</span>
+                            <Field
+                              name={`answers.${index}`}
 
-          <div style={labelInputContainer}>
-            <label htmlFor="answer4" style={label}>Answer 4</label>
-            <Field name="answer4" style={quizFormInputText} />
-            {errors.answer4 && touched.answer4 ? (
-              <div style={quizFormErrMsg}>{errors.answer4}</div>
-            ) : null}
-          </div> */}
+                              onFocus={() => {
+                                switch (index) {
+                                  case 0: return setFocused("a1");
+                                  case 1: return setFocused("a2");
+                                  case 2: return setFocused("a3");
+                                  case 3: return setFocused("a4");
+                                }
+                              }}
+                              onBlur={() => {
+                                switch (index) {
+                                  case 0: return setFocused("");
+                                  case 1: return setFocused("");
+                                  case 2: return setFocused("");
+                                  case 3: return setFocused("");
+                                }
+                              }}
+                              style={focused === "a1" && index === 0 ? focusStyle 
+                                : focused === "a2" && index === 1 ? focusStyle
+                                : focused === "a3" && index === 2 ? focusStyle 
+                                : focused === "a4" && index === 3 ? focusStyle : quizFormInputText}
+                            />
+                            {values.answers.length >= 3 ? (
+                              <i
+                                onClick={() => arrayHelpers.remove(index)}
+                                style={removeIcon}
+                              >
+                                {ioRemoveCircleOutline}
+                              </i>
+                            ) : (
+                              ''
+                            )}
+                          </div>
+                          {/* <div style={quizFormErrMsg}>
+                            <ErrorMessage name={`answers.${index}`} />
+                          </div> */}
+                          {errors.answers && touched.answers && errors.answers !== 'Duplicate answers are not allowed' ? (
+                            <div style={quizFormErrMsg}>
+                            <ErrorMessage name={`answers.${index}`} />
+                            </div>
+                          ) : null}
+                        </div>
+                      ))
+                      : null
+                    }
 
-          <div style={labelInputContainer}>
-            <label htmlFor='correctAnswer' style={label}>
-              Correct Answer
-            </label>
-            <Field name='correctAnswer' style={quizFormInputText} />
-            {errors.correctAnswer && touched.correctAnswer ? (
-              <div style={quizFormErrMsg}>Only 1 ~ 4 are allowed.</div>
-            ) : null}
-          </div>
+                </div>
+              )}
+            />
 
-          <div style={labelInputContainer}>
-            <label style={label}>Category</label>
-            <Field as='select' name='category' style={quizFormInputText}>
-              <option value='' disabled>
-                Select a category
-              </option>
-              <option value='Workout'>Workout</option>
-              <option value='Muscle'>Muscle</option>
-              <option value='Nutrition'>Nutrition</option>
-              <option value='Other'>Other</option>
-            </Field>
-            {errors.category && touched.category ? (
-              <div style={quizFormErrMsg}>{errors.category}</div>
-            ) : null}
-          </div>
+            <div style={labelInputContainer}>
+              <label htmlFor='correctAnswer' style={label}>
+                Correct Answer
+              </label>
+              <Field
+              type="number"
+                name='correctAnswer'
+                onFocus={() => {
+                  setFocused("ca");
+                }}
+                onBlur={() => {
+                  setFocused("");
+                }}
+                style={focused === "ca" ? focusStyle : quizFormInputText }
+              />
+              {errors.correctAnswer && touched.correctAnswer ? (
+                <div style={quizFormErrMsg}>
+                  Only 1 ~ {values.answers.length} are allowed.
+                </div>
+              ) : null}
+            </div>
 
-          <button type='submit' style={submitButton}>
-            Submit
-          </button>
-        </Form>
-      )}
-    </Formik>
-  </div>
-);
+            <div style={labelInputContainer}>
+              <label style={label}>Category</label>
+              <Field
+                as='select'
+                name='category'
+                onFocus={() => {
+                  setFocused("category");
+                }}
+                onBlur={() => {
+                  setFocused("");
+                }}
+                style={focused === "category" ? focusStyle : quizFormInputText }
+              >
+                <option value='' disabled>
+                  Select a category
+                </option>
+                <option value='Workout'>Workout</option>
+                <option value='Muscle'>Muscle</option>
+                <option value='Nutrition'>Nutrition</option>
+                <option value='Other'>Other</option>
+              </Field>
+              {errors.category && touched.category ? (
+                <div style={quizFormErrMsg}>{errors.category}</div>
+              ) : null}
+            </div>
+
+            <button
+              // disabled={!dirty && isValid}
+              type='submit'
+              style={submitButton}
+            >
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
 
 const labelInputContainer = {
   background: '',
+  margin: '20px 0',
 };
 
 const label = {
-  fontSize: '0.9rem',
-  marginBottom: '5px',
+  fontSize: '1.2rem',
+  margin: '10px 0 5px',
 };
 
 const quizFormInputText = {
-  marginBottom: '10px',
   display: 'block',
   width: '100%',
   flexDirection: 'column',
-  border: '1px solid rgb(200, 200, 200)',
+  border: 'none',
+  borderBottom: '1px solid rgb(200, 200, 200)',
   fontSize: '1rem',
-  padding: '10px',
-  borderRadius: '5px',
+  padding: '3px 10px',
+  marginTop: "5px",
+
+};
+const focusStyle = {
+  display: 'block',
+  width: '100%',
+  flexDirection: 'column',
+  border: 'none',
+  fontSize: '1rem',
+  padding: '3px 10px',
+  outline: 'none',
+  borderBottom: '1px solid #005bbb',
+  marginTop: "5px",
 };
 
 const quizFormErrMsg = {
   color: 'red',
   fontSize: '.9rem',
-  padding: '5px 15px',
-  background: 'rgb(255, 240, 240)',
-  // display: "none",
+  // margin: '0px 0px 20px',
+  // background: 'rgb(255, 240, 240)',
+};
+
+const answerIndex = {
+  fontSize: '1.2rem',
+  // marginRight: '10px',
+};
+
+
+// This includes an answer and error
+const answerContainer = {
+  display: 'flex',
+  // alignItems: "center",
+  flexDirection: 'column',
+};
+
+// This includes an answer index, input, and a remove icon .
+const indexAnswerIconContainer = {
+  marginTop: "10px",
+  display: 'flex',
+  alignItems: 'end',
+  gap: "10px",
+};
+
+const moreAnswerIcon = {
+  fontSize: '1rem',
+  color: '#005bbb',
+  marginLeft: '20px',
+  padding: '0px 10px',
+  // border: "1px solid #005bbb",
+  cursor: 'pointer',
+};
+
+const removeIcon = {
+  fontSize: '1.2rem',
+  color: 'red',
+  cursor: 'pointer',
+  height: '25px'
 };
 
 const submitButton = {
   padding: '10px 18px',
+  marginTop: '20px',
   borderRadius: '5px',
   border: 'none',
   width: '100%',
@@ -234,7 +310,4 @@ const submitButton = {
   background: 'rgb(245, 245, 245)',
 };
 
-const disableToClick = {
-  display: "none",
-}
 export default FormikNewQuiz;
