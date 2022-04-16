3a2611d7984607d927485d6ad8e0c70233e745f3
mtake986 <tak21011@byui.edu>
2022-04-16 16:09:50 -0500

    add a btn to navigate users to QuizHome when they face to ErrorPage

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
+  let navigate = useNavigate();
   return (
-    <div>ErrorPage</div>
+    <div>
+      <h2>Error Page</h2>
+      <button style={style} onClick={() => {navigate("/kinniku-quiz/home")}}>Go to Kinniku Quiz Home</button>
+    </div>
   )
+}
+
+const style = {
+  padding: "10px 20px",
+  margin: "0 10px 10px 0",
+  borderRadius: "5px",
+  border: "1px solid white",
+  background: "white",
+  cursor: "pointer",
+  fontSize: "1rem",
+  transition: "0.3s",
 }
 
 export default ErrorPage
